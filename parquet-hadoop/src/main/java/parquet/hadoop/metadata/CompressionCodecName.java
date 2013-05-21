b68b616b7904b66c38f953e2ce46eceb52094aea
Uri Laserson <laserson@cloudera.com>
2013-05-20 17:23:14 -0700

    Support for non-block Snappy compression

--- parquet-hadoop/src/main/java/parquet/hadoop/metadata/CompressionCodecName.java
+++ parquet-hadoop/src/main/java/parquet/hadoop/metadata/CompressionCodecName.java
@@ -19,7 +19,8 @@ import parquet.format.CompressionCodec;
 
 public enum CompressionCodecName {
   UNCOMPRESSED(null, CompressionCodec.UNCOMPRESSED, ""),
-  SNAPPY("org.apache.hadoop.io.compress.SnappyCodec", CompressionCodec.SNAPPY, ".snappy"),
+//  SNAPPY("org.apache.hadoop.io.compress.SnappyCodec", CompressionCodec.SNAPPY, ".snappy"),
+  SNAPPY("parquet.hadoop.util.RawSnappyCodec", CompressionCodec.SNAPPY, ".snappy"),
   GZIP("org.apache.hadoop.io.compress.GzipCodec", CompressionCodec.GZIP, ".gz"),
   LZO("com.hadoop.compression.lzo.LzoCodec", CompressionCodec.LZO, ".lzo");
 
---
+++ parquet-hadoop/src/main/java/parquet/hadoop/util/RawSnappyCodec.java
@@ -0,0 +1,93 @@
+package parquet.hadoop.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CommonConfigurationKeys;
+import org.apache.hadoop.io.compress.BlockCompressorStream;
+import org.apache.hadoop.io.compress.BlockDecompressorStream;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionInputStream;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.Compressor;
+import org.apache.hadoop.io.compress.CompressorStream;
+import org.apache.hadoop.io.compress.Decompressor;
+import org.apache.hadoop.io.compress.DecompressorStream;
+import org.apache.hadoop.io.compress.snappy.LoadSnappy;
+import org.apache.hadoop.io.compress.snappy.SnappyCompressor;
+import org.apache.hadoop.io.compress.snappy.SnappyDecompressor;
+
+import parquet.hadoop.ParquetWriter;
+
+public class RawSnappyCodec implements Configurable, CompressionCodec {
+  Configuration conf;
+
+  @Override
+  public void setConf(Configuration conf) {
+    this.conf = conf;
+  }
+
+  @Override
+  public Configuration getConf() {
+    return conf;
+  }
+
+  public static boolean isNativeSnappyLoaded(Configuration conf) {
+    return true;
+  }
+
+  @Override
+  public CompressionOutputStream createOutputStream(OutputStream out)
+      throws IOException {
+    return createOutputStream(out, createCompressor());
+  }
+
+  @Override
+  public CompressionOutputStream createOutputStream(OutputStream out,
+      Compressor compressor) throws IOException {
+    int bufferSize = 168 * 1024 * 1024;
+    return new CompressorStream(out, compressor, bufferSize);
+  }
+
+  @Override
+  public Class<? extends Compressor> getCompressorType() {
+    return SnappyCompressor.class;
+  }
+
+  @Override
+  public Compressor createCompressor() {
+    int bufferSize = 168 * 1024 * 1024;
+    return new SnappyCompressor(bufferSize);
+  }
+
+  @Override
+  public CompressionInputStream createInputStream(InputStream in)
+      throws IOException {
+    return createInputStream(in, createDecompressor());
+  }
+
+  @Override
+  public CompressionInputStream createInputStream(InputStream in,
+      Decompressor decompressor) throws IOException {
+    return new DecompressorStream(in, decompressor, 168 * 1024 * 1024);
+  }
+
+  @Override
+  public Class<? extends Decompressor> getDecompressorType() {
+    return SnappyDecompressor.class;
+  }
+
+  @Override
+  public Decompressor createDecompressor() {
+    int bufferSize = 168 * 1024 * 1024;
+    return new SnappyDecompressor(bufferSize);
+  }
+
+  @Override
+  public String getDefaultExtension() {
+    return ".snappy";
+  }
+}
